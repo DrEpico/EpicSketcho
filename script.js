05fc15778e5a1a46e74685464a04056741f01193
ali <adadashipour@gmail.com>
2023-01-23 17:37:38 +0000

    Changed querySelector target to #divs-container as the appended divs will be childs to the selected note and the button shouldn't be targeted.

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let userSelection;
 
-const container = document.querySelector("button");
+const container = document.querySelector("#divs-container");
 const sqr16 = document.createElement("div");
-sqr16.classList.add("#container");
+sqr16.classList.add("child-div");
 
 const buttons = document.querySelectorAll("#btn-container button");
 buttons.forEach(button => {
     button.addEventListener("click", function() {
         userSelection = this.innerHTML;
+        console.log(this.innerHTML);
         addDivs();
     })
 });
 
 function addDivs() {
     if (userSelection == "16") {
-    sqr16.textContent = "";
-    container.appendChild(sqr16);
+        console.log("userSelection value IS 16");
+        sqr16.textContent = "test";
+        container.appendChild(sqr16);
     } else {
-        console.log("userSelection value is not 16");
+        console.log("userSelection value is NOT 16");
     }
 }
