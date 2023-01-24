e20a36b380da0774b4676c8a87f2ba71665334e6
ali <adadashipour@gmail.com>
2023-01-24 18:24:22 +0000

    moved the unnamed function inside addDivs() to fix the issues around DOM manipulation not being done first and etc. This is the first version that actual lines can be drawn but without much control.

--- script.js
+++ script.js
@@ -13,15 +13,24 @@ buttons.forEach(button => {
 
 function addDivs() {
     if (userSelection === 16) {
+        container.innerHTML = "";
         for(let i=0; i<userSelection**2; i++){
             const sqr16 = document.createElement("div");
-            sqr16.classList.add("child-div");
+            sqr16.classList.add("child-divs");
             container.appendChild(sqr16); 
         }
         console.log("userSelection value IS 16");
+        const childDivs = document.querySelectorAll(".child-divs");
+        childDivs.forEach(div => {
+            div.addEventListener("mouseover", function() {
+                div.classList.add("red")
+            });
+        });
     } else {
         console.log("userSelection value is NOT 16");
     }
 }
 
-//flex direction row
+
+    
+
