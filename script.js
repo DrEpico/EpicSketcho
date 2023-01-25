720e4e58adb13e445db5bd24358870623fc18f97
ali <adadashipour@gmail.com>
2023-01-25 14:32:05 +0000

    Added if statements that change the insile CSS for max-width depending on what button is pressed.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const container = document.querySelector("#divs-container");
 const canvasbtn = document.querySelectorAll("#btn-container .canvas-size");
 canvasbtn.forEach(button => {
     button.addEventListener("click", function() {
-        userSelection = +this.innerHTML;
+        canvasSelection = +this.innerHTML;
         console.log(this.innerHTML);
         addDivs();
     })
@@ -20,28 +20,37 @@ colourbtn.forEach(button => {
     })
 });
 
-function addDivs() {
-        container.innerHTML = "";
-        for(let i=0; i<userSelection**2; i++){ //change max width depending to the button 
-            const canvas = document.createElement("div");
-            canvas.classList.add("child-divs");
-            container.appendChild(canvas); 
-        }
-        console.log("userSelection value IS 16");
+function delDivs() {
 
-        const childDivs = document.querySelectorAll(".child-divs");
-        childDivs.forEach(function(div) {
-            div.addEventListener("mouseover", function(event) {
-                if(event.which === 1) {
-                    div.classList.add(penSelection);
-                }
-            });
-            div.addEventListener("mousedown", function(event) {
-                div.classList.add(penSelection);
-            });
-        });
 }
 
+function addDivs() {
+    container.innerHTML = "";
+    for(let i=0; i<canvasSelection**2; i++){ //change max width depending to the button 
+        const canvas = document.createElement("div");
+        canvas.classList.add("child-divs");
+        container.appendChild(canvas);
+        if(canvasSelection == 16){
+            container.style.cssText = "max-width: 195px";
+        } else if(canvasSelection == 32) {
+            container.style.cssText = "max-width: 390px";
+        } else if(canvasSelection == 64) {
+            container.style.cssText = "max-width: 780px";
+        } else if(canvasSelection == 80) {
+            container.style.cssText = "max-width: 890px";
+        } 
+    console.log("userSelection value IS 16");
 
-    
-
+    const childDivs = document.querySelectorAll(".child-divs");
+    childDivs.forEach(function(div) {
+        div.addEventListener("mouseover", function(event) {
+            if(event.which === 1) {
+                div.classList.add(penSelection);
+            }
+        });
+        div.addEventListener("mousedown", function() {
+            div.classList.add(penSelection);
+        });
+    });
+    }
+}
