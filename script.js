7e84f4dc599f952c9317b1639676d4e4cf81d2ac
ali <adadashipour@gmail.com>
2023-01-25 15:38:37 +0000

    Slight restructure

--- script.js
+++ script.js
@@ -20,26 +20,21 @@ colourbtn.forEach(button => {
     })
 });
 
-function delDivs() {
-
-}
-
 function addDivs() {
     container.innerHTML = "";
-    for(let i=0; i<canvasSelection**2; i++){ //change max width depending to the button 
+    for(let i=0; i<canvasSelection**2; i++){ 
         const canvas = document.createElement("div");
         canvas.classList.add("child-divs");
         container.appendChild(canvas);
-        if(canvasSelection == 16){
+        if(canvasSelection === 16){
             container.style.cssText = "max-width: 195px";
-        } else if(canvasSelection == 32) {
+        } else if(canvasSelection === 32) {
             container.style.cssText = "max-width: 390px";
-        } else if(canvasSelection == 64) {
+        } else if(canvasSelection === 64) {
             container.style.cssText = "max-width: 780px";
-        } else if(canvasSelection == 80) {
-            container.style.cssText = "max-width: 890px";
+        } else if(canvasSelection === 80) {
+            container.style.cssText = "max-width: 980px";
         } 
-    console.log("userSelection value IS 16");
 
     const childDivs = document.querySelectorAll(".child-divs");
     childDivs.forEach(function(div) {
